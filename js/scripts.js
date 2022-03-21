6551a52bb49779698e0cc3be80d640f65ecbd539
Jake Edgar <jakeedgar1012@gmail.com>
2022-03-21 15:04:20 -0700

    add images and finalize cipher function code

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ $(document).ready(function() {
 let sentenceOne = prompt("Enter a sentence");
 
 const firstLetter = sentenceOne.charAt(0);
+
+const lastLetter = sentenceOne.charAt(sentenceOne.length -1);
+
+let cipherOne =  lastLetter + firstLetter;
+
+let sentenceTwo = sentenceOne + cipherOne;
+
+let cipherTwo = sentenceOne.charAt(sentenceOne.length/2);
+
+let sentenceThree = cipherTwo + sentenceTwo;
+
+function reverseString(str) {
+  return alert(str.split("").reverse().join(""));
+}
+reverseString(sentenceThree);
+
